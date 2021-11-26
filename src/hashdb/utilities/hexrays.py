512115df6550246e77e69dec391ec5e47c936d4a
Anthony Printup <anthony@printup.io>
2021-11-26 02:52:30 +0100

    Added a secondary helper to check if the Hex-Rays module is available - we need to import ida_hexrays if it's available.

--- src/hashdb/utilities/hexrays.py
+++ src/hashdb/utilities/hexrays.py
@@ -1,3 +1,16 @@
+def is_hexrays_module_available() -> bool:
+    """
+    Checks if the Hex-Rays decompiler is available.
+    @return: True if the Hex-Rays decompiler is available,
+             False otherwise.
+    """
+    try:
+        import ida_hexrays
+        return True
+    except ImportError:
+        return False
+
+
 def is_hexrays_decompiler_available() -> bool:
     """
     Checks if the Hex-Rays decompiler is available.
@@ -9,3 +22,4 @@ def is_hexrays_decompiler_available() -> bool:
         return ida_hexrays.init_hexrays_plugin()
     except ImportError:
         return False
+
