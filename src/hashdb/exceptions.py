7e623fa4816204f67bf4885d88a0a2577b39b083
Anthony Printup <anthony@printup.io>
2021-11-26 11:47:07 +0100

    Added multiple exceptions for specific errors.

---
+++ src/hashdb/exceptions.py
@@ -0,0 +1,42 @@
+class Exceptions:
+    """
+    This class is used as a namespace for the actual
+      exception objects.
+    """
+    class Error(Exception):
+        """
+        Used for general/non-specific errors.
+        """
+
+    class Json(Error):
+        """
+        Used when the response body
+          has an invalid json format.
+        """
+
+    class ResponseCode(Error):
+        """
+        Used when a response status code
+          isn't equal to 200 (OK)
+        """
+        response_code: int
+
+        def __init__(self, message: str, response_code: int):
+            super().__init__(message)
+            self.response_code = response_code
+
+    class Timeout(Error):
+        """
+        Used when a timeout is reached.
+        """
+
+    class InvalidAlgorithmType(Error):
+        """
+        Used when an algorithm has an
+          invalid size.
+        """
+        algorithm_type: str
+
+        def __init__(self, message: str, algorithm_type: str):
+            super().__init__(message)
+            self.algorithm_type = algorithm_type
