6cb656167549eb08ae0f896767ce1ef1b1aaecb9
Anthony Printup <anthony@printup.io>
2021-11-26 01:39:26 +0100

    Changed compressed icon types from `str` to `bytes`

--- src/hashdb/ida/ui/icons.py
+++ src/hashdb/ida/ui/icons.py
@@ -1,47 +1,47 @@
 # noinspection SpellCheckingInspection
-LOOKUP_HASH_ICON_COMPRESSED: str = r"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xh" \
-                                   r"BQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA" \
-                                   r"AmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflCRgSGCi6" \
-                                   r"7HotAAABI0lEQVQoz23RvUqbYRjG8V/ePNVkCMG0NpY3IR0siNChUOgBFAy4u3Sp" \
-                                   r"owfQuasgHoNzh1KkXUvoIi4qTkVwSlpMm6JWkMakxsclkqD+1+vm5vpgnIyCBOTk" \
-                                   r"3cO0D1aV8NYXC3cP6nr2PPTApmgFw4c3LJjw1bFnXmn7BgFBWRAVvXbpuxmLZjQM" \
-                                   r"pH5nkPqo6krWY4mOvqKic3+1vEnGEpQEJ/ryinpOZW6krCcqXjjQtaTsveizp6pS" \
-                                   r"IQjKEgOzapoO5bzEjr6oY0DNlp+a/oi6WlouRB0/bEtHMSeUXDoRTZl0pjtyEKQq" \
-                                   r"lv2377lZDdE7qZpUGFW0JlrHnCNt87crfmRPTx0rok2TIyk73K7slw3/lEz5ZPfu" \
-                                   r"TInC0FZebly4BtahTOZfSL/8AAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTA5LTI0" \
-                                   r"VDE4OjI0OjQwKzAwOjAw1ztm9QAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0wOS0y" \
-                                   r"NFQxODoyNDo0MCswMDowMKZm3kkAAABXelRYdFJhdyBwcm9maWxlIHR5cGUgaXB0" \
-                                   r"YwAAeJzj8gwIcVYoKMpPy8xJ5VIAAyMLLmMLEyMTS5MUAxMgRIA0w2QDI7NUIMvY" \
-                                   r"1MjEzMQcxAfLgEigSi4A6hcRdPJCNZUAAAAASUVORK5CYII="
+LOOKUP_HASH_ICON_COMPRESSED: bytes = r"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xh" \
+                                     r"BQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA" \
+                                     r"AmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflCRgSGCi6" \
+                                     r"7HotAAABI0lEQVQoz23RvUqbYRjG8V/ePNVkCMG0NpY3IR0siNChUOgBFAy4u3Sp" \
+                                     r"owfQuasgHoNzh1KkXUvoIi4qTkVwSlpMm6JWkMakxsclkqD+1+vm5vpgnIyCBOTk" \
+                                     r"3cO0D1aV8NYXC3cP6nr2PPTApmgFw4c3LJjw1bFnXmn7BgFBWRAVvXbpuxmLZjQM" \
+                                     r"pH5nkPqo6krWY4mOvqKic3+1vEnGEpQEJ/ryinpOZW6krCcqXjjQtaTsveizp6pS" \
+                                     r"IQjKEgOzapoO5bzEjr6oY0DNlp+a/oi6WlouRB0/bEtHMSeUXDoRTZl0pjtyEKQq" \
+                                     r"lv2377lZDdE7qZpUGFW0JlrHnCNt87crfmRPTx0rok2TIyk73K7slw3/lEz5ZPfu" \
+                                     r"TInC0FZebly4BtahTOZfSL/8AAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTA5LTI0" \
+                                     r"VDE4OjI0OjQwKzAwOjAw1ztm9QAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0wOS0y" \
+                                     r"NFQxODoyNDo0MCswMDowMKZm3kkAAABXelRYdFJhdyBwcm9maWxlIHR5cGUgaXB0" \
+                                     r"YwAAeJzj8gwIcVYoKMpPy8xJ5VIAAyMLLmMLEyMTS5MUAxMgRIA0w2QDI7NUIMvY" \
+                                     r"1MjEzMQcxAfLgEigSi4A6hcRdPJCNZUAAAAASUVORK5CYII=".encode()
 
 # noinspection SpellCheckingInspection
-HUNT_HASH_ICON_COMPRESSED: str = r"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xh" \
-                                 r"BQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA" \
-                                 r"AmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflCR0QIyJS" \
-                                 r"0VhXAAABLklEQVQoz43RO0ubARjF8Z9JqEO98Q7d0sVOSgc7aNWh8QtU1E0ULy+C" \
-                                 r"rYuDk2DxAn6ECC1iHOrm4iAIXppBUQeHUp06mUF0eBWrYLzQDml0U8/ywOHwHPif" \
-                                 r"Eg8qUSFA5NzfB7N4a3V65wWu7VmwXwgVAgk9BmzacImXWjT7Zt4tCdAjNKFK7/+K" \
-                                 r"RavGMFv48NZXk977YM4hXuuTte2LAb/iGHbkVLtRzbqkXEnrd+BOjbWYSnV+aJPR" \
-                                 r"qtq4cdU+ymiTVacyJlDqQiCn3oxBg2Y0yAn8USqIeUIJkbwykaRdn6Tx2Y6kSLm8" \
-                                 r"KC6v1hvrQmmvdEv5acmI7xrlLMdxbMiKO6FVS7JujMj6rcu0kwLJUGhKlY57UGfG" \
-                                 r"zBZBFVFv2XCBMi2aiqifOdYjc/8Dq21eq6/oesAAAAAldEVYdGRhdGU6Y3JlYXRl" \
-                                 r"ADIwMjEtMDktMjlUMTY6MzU6MzQrMDA6MDD0UbHoAAAAJXRFWHRkYXRlOm1vZGlm" \
-                                 r"eQAyMDIxLTA5LTI5VDE2OjM1OjM0KzAwOjAwhQwJVAAAAFd6VFh0UmF3IHByb2Zp" \
-                                 r"bGUgdHlwZSBpcHRjAAB4nOPyDAhxVigoyk/LzEnlUgADIwsuYwsTIxNLkxQDEyBE" \
-                                 r"gDTDZAMjs1Qgy9jUyMTMxBzEB8uASKBKLgDqFxF08kI1lQAAAABJRU5ErkJggg=="
+HUNT_HASH_ICON_COMPRESSED: bytes = r"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xh" \
+                                   r"BQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA" \
+                                   r"AmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflCR0QIyJS" \
+                                   r"0VhXAAABLklEQVQoz43RO0ubARjF8Z9JqEO98Q7d0sVOSgc7aNWh8QtU1E0ULy+C" \
+                                   r"rYuDk2DxAn6ECC1iHOrm4iAIXppBUQeHUp06mUF0eBWrYLzQDml0U8/ywOHwHPif" \
+                                   r"Eg8qUSFA5NzfB7N4a3V65wWu7VmwXwgVAgk9BmzacImXWjT7Zt4tCdAjNKFK7/+K" \
+                                   r"RavGMFv48NZXk977YM4hXuuTte2LAb/iGHbkVLtRzbqkXEnrd+BOjbWYSnV+aJPR" \
+                                   r"qtq4cdU+ymiTVacyJlDqQiCn3oxBg2Y0yAn8USqIeUIJkbwykaRdn6Tx2Y6kSLm8" \
+                                   r"KC6v1hvrQmmvdEv5acmI7xrlLMdxbMiKO6FVS7JujMj6rcu0kwLJUGhKlY57UGfG" \
+                                   r"zBZBFVFv2XCBMi2aiqifOdYjc/8Dq21eq6/oesAAAAAldEVYdGRhdGU6Y3JlYXRl" \
+                                   r"ADIwMjEtMDktMjlUMTY6MzU6MzQrMDA6MDD0UbHoAAAAJXRFWHRkYXRlOm1vZGlm" \
+                                   r"eQAyMDIxLTA5LTI5VDE2OjM1OjM0KzAwOjAwhQwJVAAAAFd6VFh0UmF3IHByb2Zp" \
+                                   r"bGUgdHlwZSBpcHRjAAB4nOPyDAhxVigoyk/LzEnlUgADIwsuYwsTIxNLkxQDEyBE" \
+                                   r"gDTDZAMjs1Qgy9jUyMTMxBzEB8uASKBKLgDqFxF08kI1lQAAAABJRU5ErkJggg==".encode()
 
 # noinspection SpellCheckingInspection
-SCAN_HASHES_ICON_COMPRESSED: str = r"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xh" \
-                                   r"BQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA" \
-                                   r"AmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflCggXHAT9" \
-                                   r"KjxuAAABI0lEQVQoz2XRsUviARjG8Y/2y+vQBMcgqElpiaLtfhbEbda/0Bjt0W0H" \
-                                   r"Ri1FQ0ugS8Q5Xw1BQ2sQ3BIEkXFS0tBQU9egaJA16IXZ9x3fZ3jf50ubQKioomFP" \
-                                   r"ny4CkLJsyYCycydaekgpadoXiouJ9q6j1jVtiMtYdWSlNxJ68FvCvKq6Sz97A0VP" \
-                                   r"pmVcu5ST/Hgi/PVHQl5NTsqKH4YM+NqZ/sCwc8+mVJ1atOXFgkctEVEt1cBnXn0x" \
-                                   r"6Z+KcROBOyNiznyXtYtXv8wqKdhxLEHh/ciynKSkOVeupc14UiB033nzRt2FC3U3" \
-                                   r"5g06cC/sLiot79ChvLSETU1r7U7+V50VFxMTl3WgqSRF5IOsK7cYNaahaNtjt9W2" \
-                                   r"7pqaiqJvHc/eAJ9oWcCbnateAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTEwLTA4" \
-                                   r"VDIzOjI4OjA0KzAwOjAw7pDTfgAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0xMC0w" \
-                                   r"OFQyMzoyODowNCswMDowMJ/Na8IAAABXelRYdFJhdyBwcm9maWxlIHR5cGUgaXB0" \
-                                   r"YwAAeJzj8gwIcVYoKMpPy8xJ5VIAAyMLLmMLEyMTS5MUAxMgRIA0w2QDI7NUIMvY" \
-                                   r"1MjEzMQcxAfLgEigSi4A6hcRdPJCNZUAAAAASUVORK5CYII="
+SCAN_HASHES_ICON_COMPRESSED: bytes = r"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xh" \
+                                     r"BQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA" \
+                                     r"AmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQflCggXHAT9" \
+                                     r"KjxuAAABI0lEQVQoz2XRsUviARjG8Y/2y+vQBMcgqElpiaLtfhbEbda/0Bjt0W0H" \
+                                     r"Ri1FQ0ugS8Q5Xw1BQ2sQ3BIEkXFS0tBQU9egaJA16IXZ9x3fZ3jf50ubQKioomFP" \
+                                     r"ny4CkLJsyYCycydaekgpadoXiouJ9q6j1jVtiMtYdWSlNxJ68FvCvKq6Sz97A0VP" \
+                                     r"pmVcu5ST/Hgi/PVHQl5NTsqKH4YM+NqZ/sCwc8+mVJ1atOXFgkctEVEt1cBnXn0x" \
+                                     r"6Z+KcROBOyNiznyXtYtXv8wqKdhxLEHh/ciynKSkOVeupc14UiB033nzRt2FC3U3" \
+                                     r"5g06cC/sLiot79ChvLSETU1r7U7+V50VFxMTl3WgqSRF5IOsK7cYNaahaNtjt9W2" \
+                                     r"7pqaiqJvHc/eAJ9oWcCbnateAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTEwLTA4" \
+                                     r"VDIzOjI4OjA0KzAwOjAw7pDTfgAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0xMC0w" \
+                                     r"OFQyMzoyODowNCswMDowMJ/Na8IAAABXelRYdFJhdyBwcm9maWxlIHR5cGUgaXB0" \
+                                     r"YwAAeJzj8gwIcVYoKMpPy8xJ5VIAAyMLLmMLEyMTS5MUAxMgRIA0w2QDI7NUIMvY" \
+                                     r"1MjEzMQcxAfLgEigSi4A6hcRdPJCNZUAAAAASUVORK5CYII=".encode()
