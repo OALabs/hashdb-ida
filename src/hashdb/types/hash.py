7b7f79332cc3181476b42b681116e88f111a586c
Anthony Printup <anthony@printup.io>
2021-11-27 04:10:52 +0100

    Modified the `Hash` class to allow default variables.

--- src/hashdb/types/hash.py
+++ src/hashdb/types/hash.py
@@ -6,9 +6,12 @@ class Hash(NamedTuple):
     """
     Interface for a hash object.
     """
-    hash: int              # hash value
-    string: str            # hashed string
-    is_api: bool           # True if the hash is an API
-    permutation_type: str  # type of permutation
-    api: str               # hashed API string
-    modules: tuple[str]    # a tuple of module strings associated to the hash
+    value: int                    # hash value
+    string: str                   # hashed string
+    is_api: bool                  # True if the hash is an API
+
+    # The remaining  arguments are optional
+    #  (strings that aren't APIs)
+    permutation_type: str = None  # type of permutation
+    api: str = None               # hashed API string
+    modules: tuple[str] = None    # a tuple of module strings associated to the hash
