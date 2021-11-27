71ffdf9f43f64b5eeef7aea3b48a7e33f335fbfb
Anthony Printup <anthony@printup.io>
2021-11-27 10:23:28 +0100

    Changed minimum version requirements: - IDA 7.0 -> 7.5, - Python 3.6 -> 3.8 (bundled with IDA 7.5).

--- src/hashdb/utilities/versions.py
+++ src/hashdb/utilities/versions.py
@@ -8,22 +8,22 @@ import ida_kernwin
 def is_python_version_supported() -> bool:
     """
     Checks if the Python version number is higher or
-      equal to 3.6
-    @return: sys.version_info >= 3.6
+      equal to 3.8
+    @return: sys.version_info >= 3.8
     """
     minimum_major = 3
-    minimum_minor = 6
+    minimum_minor = 8
     return sys.version_info >= (minimum_major, minimum_minor)
 
 
 def is_ida_version_supported() -> bool:
     """
     Checks if the IDA kernel version is higher or
-      equal to 7.0
-    @return: ida_kernel_version >= 7.0
+      equal to 7.5
+    @return: ida_kernel_version >= 7.5
     """
     minimum_major = 7
-    minimum_minor = 0
+    minimum_minor = 5
 
     ida_kernel_version = tuple(map(int, ida_kernwin.get_kernel_version().split(".")))
     return ida_kernel_version >= (minimum_major, minimum_minor)
