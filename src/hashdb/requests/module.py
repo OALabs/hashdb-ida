32f84594d406b31c535084a0a68e376a92928b50
Anthony Printup <anthony@printup.io>
2021-11-27 04:20:13 +0100

    Fixed indentations.

--- src/hashdb/requests/module.py
+++ src/hashdb/requests/module.py
@@ -10,18 +10,18 @@ from ..types.hash import Hash
 def fetch(api_url: str, timeout: int,
           module_name: str, algorithm_name: str, permutation_type: str) -> dict:
     """
-        Fetches all hash matches/hits from the server.
-        @param api_url: api url to use
-        @param timeout: amount of seconds before we timeout
-        @param module_name: name of the module
-        @param algorithm_name: name of the algorithm
-        @param permutation_type: permutation type
-        @return: a json object (dict) fetched from the server
-                 containing a list of hash objects
-        @raise Exceptions.Timeout: if a request timed out
-        @raise Exceptions.ResponseCode: if an unexpected status code is encountered,
-        @raise Exceptions.Json: if the response body isn't valid JSON.
-        """
+    Fetches all hash matches/hits from the server.
+    @param api_url: api url to use
+    @param timeout: amount of seconds before we timeout
+    @param module_name: name of the module
+    @param algorithm_name: name of the algorithm
+    @param permutation_type: permutation type
+    @return: a json object (dict) fetched from the server
+             containing a list of hash objects
+    @raise Exceptions.Timeout: if a request timed out
+    @raise Exceptions.ResponseCode: if an unexpected status code is encountered,
+    @raise Exceptions.Json: if the response body isn't valid JSON.
+    """
     # Perform the request
     url = f"{api_url}/module/{module_name}/{algorithm_name}/{permutation_type}"
     response = get_request(url, timeout=timeout)
