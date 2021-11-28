38e48ffc3780a2ca9f72bac44041e7fe96f6cd26
Anthony Printup <anthony@printup.io>
2021-11-28 13:18:00 +0100

    Updated docstrings.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -6,8 +6,7 @@ from hashdb.exceptions import Exceptions
 
 def test_parse_valid_hashes(valid_hash_data: dict):
     """
-    Simulate handling a valid response from:
-      https://hashdb.openanalysis.net/module/zipfldr/crc32/api
+    Simulate parsing valid hash objects.
     """
     hash_data: dict
     for index, hash_data in enumerate(valid_hash_data["hashes"]):
@@ -18,7 +17,7 @@ def test_parse_valid_hashes(valid_hash_data: dict):
 
 def test_parse_invalid_hashes(invalid_hash_data: dict):
     """
-    Simulate handling multiple invalid hash objects.
+    Simulate parsing multiple invalid hash objects.
     """
     # Should throw an exception
     for hash_data in invalid_hash_data["hashes"]:
