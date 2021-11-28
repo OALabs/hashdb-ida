dde5635a93125e3f62cc3748220b1829f0c44a30
Anthony Printup <anthony@printup.io>
2021-11-28 13:15:28 +0100

    Refactored `Hash.from_json`: - renamed `data` variable to `json`, - added more runtime type/value checks (an exception will be thrown if the json data is invalid).

--- src/hashdb/types/hash.py
+++ src/hashdb/types/hash.py
@@ -20,36 +20,47 @@ class Hash(NamedTuple):
     modules: tuple[str] = ()    # a tuple of module strings associated to the hash
 
     @classmethod
-    def from_json(cls, data: dict):
+    def from_json(cls, json: dict):
         """
         Creates a new class instance from a dictionary
-        @param data: a json object
+        @param json: a json object (dict)
         @return: a new class instance
         @raise Exceptions.InvalidHashObject: if a hash object is missing
                                              the "string" object
         """
         # Parse the data
-        value: int = data.get("hash")
-        string_object: dict = data.get("string")
+        value: int = json.get("hash")
+        if value is None:  # None
+            raise Exceptions.InvalidHashObject("Missing hash value.", hash_object=json)
+        string_object: dict = json.get("string")
+        if not string_object:  # None or empty
+            raise Exceptions.InvalidHashObject("Invalid \"string\" object.", hash_object=json)
 
-        # Check if the string object is valid
-        if not string_object:
-            raise Exceptions.InvalidHashObject("\"string\" object doesn't exist, or is empty.", hash_object=data)
-
-        string: str = string_object.get("string")
+        string_value: str = string_object.get("string")
+        if not string_value:  # None or empty string
+            raise Exceptions.InvalidHashObject("Invalid string value.", hash_object=json)
         is_api: bool = string_object.get("is_api")
+        if is_api is None:
+            raise Exceptions.InvalidHashObject("Missing is_api value.", hash_object=json)
 
         # If the hash is not an API, append a Hash instance,
         #  and skip resolving other properties:
         if not is_api:
-            return cls(value=value, string=string, is_api=is_api)
+            return cls(value=value, string=string_value, is_api=is_api)
 
-        # Resolve the remaining properties
+        # Resolve the remaining properties, and check their values
         permutation_type: str = string_object.get("permutation")
+        if not permutation_type:  # None or empty string
+            raise Exceptions.InvalidHashObject("Invalid permutation type.", hash_object=json)
         api: str = string_object.get("api")
+        if not api or api != string_value:  # None or empty string or api != string
+            raise Exceptions.InvalidHashObject("Invalid api name, or api doesn't match the raw string.",
+                                               hash_object=json)
         modules: list = string_object.get("modules")
+        if not modules or not len(modules):  # None or empty list
+            raise Exceptions.InvalidHashObject("Missing modules.", hash_object=json)
 
         # Append a Hash instance
-        return cls(value=value, string=string, is_api=is_api,
+        return cls(value=value, string=string_value, is_api=is_api,
                    permutation_type=permutation_type, api=api,
                    modules=tuple(modules))
