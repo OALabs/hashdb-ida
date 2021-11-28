c614fa3a084f88a1e596605107814a146f8e13bb
Anthony Printup <anthony@printup.io>
2021-11-28 14:22:42 +0100

    Added more tests with more cases, renamed `requests` to `parsing` to better indicate the objective.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,25 @@ import pytest
 
 
 @pytest.fixture(scope="session")
+def valid_algorithms_data():
+    yield {'algorithms': [
+        {'algorithm': 'crc32', 'description': 'Standard crc32 hash.', 'type': 'unsigned_int'},
+        {'algorithm': 'fnv1a_64', 'description': 'FNV1a hash (64-bit)', 'type': 'unsigned_long'}]}
+
+
+@pytest.fixture(scope="session")
+def invalid_algorithms_data():
+    yield {'algorithms': [
+        {},  # empty object
+        #  missing description
+        {'algorithm': 'crc32'},
+        #  missing type
+        {'algorithm': 'crc32', 'description': 'Standard crc32 hash.'},
+        #  invalid type
+        {'algorithm': 'crc32', 'description': 'Standard crc32 hash.', 'type': 'unsigned_float'}]}
+
+
+@pytest.fixture(scope="session")
 def valid_hash_data():
     yield {'hashes': [{'hash': 1653273962, 'string': {'string': 'RouteTheCall', 'is_api': True,
                                                       'permutation': 'api', 'api': 'RouteTheCall',
@@ -11,19 +30,41 @@ def valid_hash_data():
 
 @pytest.fixture(scope="session")
 def invalid_hash_data():
-    yield {'hashes': [{'hash': 1653273962},  # missing string object
-                      {'hash': 1075368562, 'string':  # missing "is_api" in the string object
+    yield {'hashes': [{},  # empty object
+                      # missing string object
+                      {'hash': 1653273962},
+                      # missing "is_api" in the string object
+                      {'hash': 1075368562, 'string':
                           {'string': 'DllGetClassObject'}},
-                      {'hash': 1075368562, 'string':  # missing permutation value
+                      # missing permutation value
+                      {'hash': 1075368562, 'string':
                           {'string': 'DllGetClassObject',
                            'is_api': True}},
-                      {'hash': 1075368562, 'string':  # api value != string value
+                      # api value != string value
+                      {'hash': 1075368562, 'string':
                           {'string': 'DllGetClassObject',
                            'is_api': True,
                            'permutation': 'api',
                            'api': 'DllGetClassObject_12345'}},
-                      {'hash': 1075368562, 'string':  # missing "modules"
+                      # missing "modules"
+                      {'hash': 1075368562, 'string':
                           {'string': 'DllGetClassObject',
                            'is_api': True,
                            'permutation': 'api',
                            'api': 'DllGetClassObject'}}]}
+
+
+@pytest.fixture(scope="session")
+def valid_hits_data():
+    yield {'hits': [{'algorithm': 'crc32', 'count': 1, 'hitrate': 1.0}]}
+
+
+@pytest.fixture(scope="session")
+def invalid_hits_data():
+    yield {'hits': [
+        {},  # empty object
+        # missing count
+        {'algorithm': 'crc32'},
+        # missing hitrate
+        {'algorithm': 'crc32', 'count': 1}
+    ]}
