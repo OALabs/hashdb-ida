7f6ab747849310d1b6ae4201d7eab277e9a4f763
Anthony Printup <anthony@printup.io>
2021-11-28 13:38:38 +0100

    Refactored `Hash.from_json` code.

--- src/hashdb/types/hash.py
+++ src/hashdb/types/hash.py
@@ -27,39 +27,31 @@ class Hash(NamedTuple):
         @return: a new class instance
         @raise Exceptions.InvalidHash: if a hash object is invalid
         """
-        # Parse the data
-        value: int = json.get("hash")
-        if value is None:  # None
-            raise Exceptions.InvalidHash("Missing hash value.", hash_object=json)
-        string_object: dict = json.get("string")
-        if not string_object:  # None or empty
-            raise Exceptions.InvalidHash("Invalid \"string\" object.", hash_object=json)
+        try:
+            value: int = json["hash"]
+            string_object: dict = json["string"]
+            string_value: str = string_object["string"]
+            is_api: bool = string_object["is_api"]
 
-        string_value: str = string_object.get("string")
-        if not string_value:  # None or empty string
-            raise Exceptions.InvalidHash("Invalid string value.", hash_object=json)
-        is_api: bool = string_object.get("is_api")
-        if is_api is None:
-            raise Exceptions.InvalidHash("Missing is_api value.", hash_object=json)
+            # If the hash is not an API, append a Hash instance,
+            #  and skip resolving other properties:
+            if not is_api:
+                return cls(value=value, string=string_value, is_api=is_api)
 
-        # If the hash is not an API, append a Hash instance,
-        #  and skip resolving other properties:
-        if not is_api:
-            return cls(value=value, string=string_value, is_api=is_api)
+            # Resolve the remaining properties, and check their values
+            permutation_type: str = string_object["permutation"]
+            api_value: str = string_object["api"]
+            modules: list = string_object["modules"]
 
-        # Resolve the remaining properties, and check their values
-        permutation_type: str = string_object.get("permutation")
-        if not permutation_type:  # None or empty string
-            raise Exceptions.InvalidHash("Invalid permutation type.", hash_object=json)
-        api: str = string_object.get("api")
-        if not api or api != string_value:  # None or empty string or api != string
-            raise Exceptions.InvalidHash("Invalid api name, or api doesn't match the raw string.",
-                                         hash_object=json)
-        modules: list = string_object.get("modules")
-        if not modules or not len(modules):  # None or empty list
-            raise Exceptions.InvalidHash("Missing modules.", hash_object=json)
+            # Check to make sure the api value and modules are valid
+            if api_value != string_value:
+                raise Exceptions.InvalidHash("Raw string and API string mismatch.", hash_object=json)
+            if not len(modules):
+                raise Exceptions.InvalidHash("Missing modules.", hash_object=json)
+        except KeyError as exception:
+            raise Exceptions.InvalidHash(f"Missing key: {exception.args[0]}", hash_object=json)
 
         # Return a Hash instance
         return cls(value=value, string=string_value, is_api=is_api,
-                   permutation_type=permutation_type, api=api,
+                   permutation_type=permutation_type, api=api_value,
                    modules=tuple(modules))
