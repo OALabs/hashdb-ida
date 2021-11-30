ec825ea99a89a6393681763f0bef7ff146bc934d
Anthony Printup <anthony@printup.io>
2021-11-30 06:32:12 +0100

    Added a new file in utilities for IDA API specific functions: - get_user_directory_path

---
+++ src/hashdb/utilities/ida.py
@@ -0,0 +1,12 @@
+# IDAPython
+import ida_diskio
+
+
+def get_user_directory_path() -> str:
+    """
+    Returns the IDA user directory path usually located at:
+      - $HOME/.idapro
+      - %APPDATA%Hex-Rays/IDA Pro
+    @return: the user directory path
+    """
+    return ida_diskio.get_user_idadir()
