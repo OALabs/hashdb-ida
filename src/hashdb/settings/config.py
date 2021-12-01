522931d9529fa7d0ccbd56e46531f290786494a1
Anthony Printup <anthony@printup.io>
2021-12-01 11:19:42 +0100

    Removed TODOs.

--- src/hashdb/settings/config.py
+++ src/hashdb/settings/config.py
@@ -147,16 +147,3 @@ def load_settings():
     except (OSError, Exceptions.Json, Exceptions.InvalidSettings) as exception:
         raise Exceptions.LoadSettingsFailure(
             f"Failed to load settings from the netnode and disk: {exception=}")
-
-
-# TODO (printup): move these TODOs to git issues :)
-
-# TODO (printup): the default API_URL should be modifiable by the user
-#                 the user should have an option to be able to set it,
-#                 at which point the API_URL would be saved in a global
-#                 config file with the following API:
-#                 ida_diskio.get_user_idadir
-# TODO (printup): the same applies for the ENUM_PREFIX and REQUEST_TIMEOUT
-# TODO (printup): all of these variables should also be savable locally,
-#                 which would be the preferred way (higher priority) of
-#                 fetching user settings
