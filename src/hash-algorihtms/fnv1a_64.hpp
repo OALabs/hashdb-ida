a937f66b6075ba5e25ac48f930728abd8ea4d123
Anthony Printup <92564080+anthonyprintup@users.noreply.github.com>
2022-08-13 10:10:37 +0200

    Added an Fnv-1A implementation.

---
+++ src/hash-algorihtms/fnv1a_64.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../utilities/strings.hpp"
+
+namespace hash_algorithms::fnv1a_64 {
+    using UnderlyingType = std::uint64_t;
+    using SeedType = std::uint64_t;
+
+    constexpr UnderlyingType prime {0x100000001B3};
+    constexpr UnderlyingType offset_basis {0xCBF29CE484222325};
+
+    template<utilities::IsCharacter T, T... Characters>
+    consteval auto calculate(const SeedType seed = {}) noexcept {
+        auto hash = offset_basis + seed;
+        (((hash ^= static_cast<UnderlyingType>(Characters)) *= prime + seed), ...);
+        return hash;
+    }
+
+    template<utilities::IsCharacter T, T... Characters>
+    consteval auto calculate(const std::integer_sequence<T, Characters...>, const SeedType seed = {}) noexcept {
+        return calculate<T, Characters...>(seed);
+    }
+
+    template<utilities::IsCharacter T>
+    [[gnu::always_inline]] constexpr auto calculate(const std::basic_string_view<T> input, const SeedType seed = {}) noexcept {
+        auto hash = offset_basis + seed;
+        for (auto &&character : input) {
+            hash ^= character;
+            hash *= prime + seed;
+        }
+        return hash;
+    }
+
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wgnu-string-literal-operator-template"
+    template<utilities::IsCharacter T, T... Characters>
+    consteval auto operator""_fnv1a_64() noexcept { return calculate<T, Characters...>(); }
+    #pragma clang diagnostic pop
+}
